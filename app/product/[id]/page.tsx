32bed0aa7b34d0873efb8d4fc88f5fb2175e9787
Abubakr-Sanginov <sanginovabubakr2222@gmail.com>
2025-08-14 11:57:22 +0500

    update

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,5 @@
-export default async function ProductPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function ProductPage({ params : {id} }: { params: { id: string } }) {
+  const product = await prisma.product.findFirst({where: {id: Number(id)}});
 
   return <p>Product {id}</p>;
 }
